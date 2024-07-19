d9f6455ae21738283c2917d94248220cdac5c5ba
seanrandevs <93683781+seanrandevs@users.noreply.github.com>
2024-07-19 13:16:59 -0600

    First commit
    80% done.
    Have some Javascript left to write and fix a couple mistakes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './header/header.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, EducationComponent, ExperienceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
