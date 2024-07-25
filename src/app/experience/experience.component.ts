547d507fecfee7a1ae23cb413275c1b64ff1ae77
seanrandevs <93683781+seanrandevs@users.noreply.github.com>
2024-07-25 16:16:00 -0600

    Fixed the Javascript

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -7,12 +7,25 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   standalone: true,
   imports: [FontAwesomeModule],
   templateUrl: './experience.component.html',
-  styleUrl: './experience.component.css'
+  styleUrls: ['./experience.component.css']
 })
 export class ExperienceComponent {
 
   faChevronDown = faChevronDown;
-  
 
-  
+  toggleDescription(id: string): void {
+    const element = document.getElementById(id);
+    if (element) {
+      if (element.style.display === 'block') {
+        element.style.removeProperty('display');
+      } else {
+        element.style.display = 'block';
+      }
+    }
+  }
 }
+
+
+// toggleActive(): void {
+  //   this.isActive = !this.isActive; // Toggle the active state
+  // }
